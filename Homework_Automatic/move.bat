ed87015c39ff1dd22b6bef2396669191276f2750
nbwzx <nbwzx@126.com>
2021-02-24 23:04:29 +0800

    update

--- Homework_Automatic/move.bat
+++ Homework_Automatic/move.bat
@@ -1,5 +1,4 @@
 @echo off
 setlocal enabledelayedexpansion
-for /L %%i in (1,1,15) do if not exist %%i mkdir %%i & xcopy 0 %%i & ren %%i\*_0.* *_%%i.*  & goto :ok
+for /L %%i in (1,1,50) do if not exist %%i mkdir %%i & xcopy 0 %%i & ren %%i\*_0.* *_%%i.*  & goto :ok
 :ok
-pause
